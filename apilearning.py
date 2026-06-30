d85202545d74b5665f0bfc2c7e68dbf1dda2a598
Vonn <miles.giftunique@gmail.com>
2026-06-30 08:56:11 -0600

    Practicing python with Alex the Analyst's tutorial

--- apilearning.py
+++ apilearning.py
@@ -1,4 +1,9 @@
 import requests
-import json
-res = requests.get('https://wtvehiclesapi.duckdns.org/docs/')
-response = json.load(res.text)
+
+url = "https://wtvehiclesapi.duckdns.org/api/vehicles/stats?version=2.53.0.7"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.headers["Content-Type"])
+print(response.text)
